5cd38b404b5fe7c3f65f84609bfca53829191677
unknown <dwuarsidawna@protonmail.com>
2023-10-02 09:08:40 -0500

    reformatted test.py

--- Tests/test.py
+++ Tests/test.py
@@ -5,20 +5,29 @@ class TestFeatures(unittest.TestCase):
     def setUp(self):
         #Load data before any test start
         load_data()
+        
 
     def test_case_justice_votes_1(self):
         #first test for the case_justice_votes feature
         vote_list = case_justice_votes("329 U.S. 1")
-        expected_list = [('HHBurton', '2'), ('RHJackson', '1'), ('WODouglas', '1'), ('FFrankfurter', '4'), ('SFReed', '1'), ('HLBlack', '1'), ('WBRutledge', '1'), ('FMurphy', '1'), ('FMVinson', '1')]
+        expected_list = [('HHBurton', '2'), ('RHJackson', '1'),
+                         ('WODouglas', '1'), ('FFrankfurter', '4'),
+                         ('SFReed', '1'), ('HLBlack', '1'),
+                         ('WBRutledge', '1'), ('FMurphy', '1'), ('FMVinson', '1')]
         self.assertEqual(vote_list,expected_list)
+        
         
     def test_case_justice_votes_2(self):
         #second test for the case_justice_votes feature
         vote_list = case_justice_votes("329 U.S. 143")
-        expected_list = [('HHBurton', '1'), ('RHJackson', '1'), ('WODouglas', '1'), ('FFrankfurter', '1'), ('SFReed', '1'), ('HLBlack', '1'), ('WBRutledge', '1'), ('FMurphy', '1'), ('FMVinson', '1')]
+        expected_list = [('HHBurton', '1'), ('RHJackson', '1'),
+                         ('WODouglas', '1'), ('FFrankfurter', '1'),
+                         ('SFReed', '1'), ('HLBlack', '1'),
+                         ('WBRutledge', '1'), ('FMurphy', '1'), ('FMVinson', '1')]
         self.assertEqual(vote_list,expected_list)
             
-    def test_edge_case_justice_votes(self):
+            
+    def test_no_id_justice_votes(self):
         #test the edge cases for case_justice_votes feature
         with self.assertRaises(LookupError):
             case_justice_votes("tests")
@@ -30,10 +39,12 @@ class TestFeatures(unittest.TestCase):
         expected_name = "UNITED STATES v. ALCEA BAND OF TILLAMOOKS ET AL."
         self.assertEqual(case_name,expected_name)
         
-    def test_edge_case_name_lookup(self):
+        
+    def test_no_id_name_lookup(self):
         #test edge case for the case_name_lookup feature
         with self.assertRaises(LookupError):
             case_name_lookup("asdf")
+           
            
 if __name__ == '__main__':
     unittest.main()
