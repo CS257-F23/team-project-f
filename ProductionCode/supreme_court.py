fca26abef3fbe32cf56809ffca0b003d59d3fb0b
unknown <dwuarsidawna@protonmail.com>
2023-10-09 20:33:56 -0500

    updated docstrings

--- ProductionCode/supreme_court.py
+++ ProductionCode/supreme_court.py
@@ -30,8 +30,8 @@ def load_data():
 def case_name_lookup(us_cite_id: str) -> str:
 
     """
-    Takes U.S. Citation as input and returns the corresponding full case name.
-    Raises LookupError if U.S. Citation is not valid/found.
+    Takes U.S. Reporter Citation as input and returns the corresponding full case name.
+    Raises LookupError if U.S. Reporter Citation is not valid/found.
     """
 
     case_name = None
@@ -39,9 +39,10 @@ def case_name_lookup(us_cite_id: str) -> str:
     for row in court_data_list[1:]:
         if row[indexer["usCite"]] == us_cite_id:
             case_name = row[indexer["caseName"]]
+            break
             
     if case_name == None:
-        raise LookupError("U.S. Citation ID not found")
+        raise LookupError("U.S. Reporter Citation ID not found")
     else:
         return case_name
 
@@ -49,8 +50,8 @@ def case_name_lookup(us_cite_id: str) -> str:
 def case_justice_votes(us_cite_id: str) -> list:
 
     """
-    Takes U.S. Citation as input and returns a list of tuples containing justice name and how they voted.
-    Raises LookupError if U.S. Citation is not valid/found.
+    Takes U.S. Reporter Citation as input and returns a list of tuples containing justice name and how they voted.
+    Raises LookupError if U.S. Reporter Citation is not valid/found.
     """
 
     case_votes = []
@@ -62,12 +63,36 @@ def case_justice_votes(us_cite_id: str) -> list:
             case_votes.append((justice_name, justice_vote))
     
     if case_votes == []:
-        raise LookupError("U.S. Citation ID not found")
+        raise LookupError("U.S. Reporter Citation ID not found")
     else:
         return case_votes
 
 
-def argument_logic(find_name: bool, find_justice_votes: bool, us_cite_id: str):
+def case_identifier_lookup(us_cite_id: str) -> dict:
+
+    """
+    Takes U.S. Reporter Citation as input and returns a dictionary of all case identifiers
+    Raises LookupError if U.S. Reporter Citation is not valid/found.
+    """
+    
+    ids = {}
+    
+    for row in court_data_list[1:]:
+        if row[indexer["usCite"]] == us_cite_id:
+            ids["U.S. Reporter"] = us_cite_id
+            ids["Supreme Court Reporter"] = row[indexer["sctCite"]]
+            ids["Lawyers' Edition Reports"] = row[indexer["ledCite"]]
+            ids["LEXIS"] = row[indexer["lexisCite"]]
+            ids["Case Name"] = row[indexer["caseName"]]
+            break
+    
+    if ids == {}:
+        raise LookupError("U.S. Reporter Citation ID not found")
+    else:
+        return ids
+ 
+
+def argument_logic(find_name: bool, find_justice_votes: bool, find_all_identifiers: bool, us_cite_id: str):
 
     """
     Handles cli argument logic and calls corresponding functions.
@@ -79,6 +104,10 @@ def argument_logic(find_name: bool, find_justice_votes: bool, us_cite_id: str):
     if find_justice_votes and us_cite_id:
         for justice in case_justice_votes(us_cite_id):
             print(" - ".join(justice))
+            
+    if find_all_identifiers and us_cite_id:
+        for key, value in case_identifier_lookup(us_cite_id).items():
+            print(key + ": " + value)
             
 
 if __name__ == "__main__":
@@ -96,13 +125,16 @@ if __name__ == "__main__":
     parser.add_argument('--find_justice_votes', action='store_true',
                         help='Finds the votes for each justice of a case')
                         
+    parser.add_argument('--find_all_identifiers', action='store_true',
+                        help='Finds all identifiers of a case')                    
+                        
     parser.add_argument('--us_citation', type=str,
-                        required=('--find_name' in sys.argv or '--find_justice_votes' in sys.argv),
-                        help='U.S. Citation ID; required  if options that involve lookups by case are used')
+                        required=('--find_name' in sys.argv or '--find_justice_votes' in sys.argv or '--find_all_identifiers' in sys.argv),
+                        help='U.S. Reporter Citation ID; required  if options that involve lookups by case are used')
                         
     args = parser.parse_args(args=(sys.argv[1:] or ['-h']))
     
     load_data()
     
-    argument_logic(args.find_name, args.find_justice_votes, args.us_citation)
+    argument_logic(args.find_name, args.find_justice_votes, args.find_all_identifiers, args.us_citation)
         
--- Tests/test_flask.py
+++ Tests/test_flask.py
@@ -1,58 +1,119 @@
 import unittest
 from flask import Flask
-from ProductionCode import supreme_court
+from ProductionCode.supreme_court import *
 from app import *
 
 class TestSupremeCourtApp(unittest.TestCase):
+
     def setUp(self):
-        # Set up a test client for making HTTP requests
+    
+        """
+        Set up test: load dataset and flask app.
+        """
+    
         load_data()
         self.app = app.test_client()
 
-    # Unit tests for display_find_name function
+
     def test_display_find_name_valid(self):
+    
+        """
+        Test correct display value for find name function.
+        """
+    
         result = display_find_name("410 U.S. 113")
         self.assertEqual(result, "ROE et al. v. WADE, DISTRICT ATTORNEY OF DALLAS COUNTY") 
 
+
     def test_display_find_name_invalid(self):
+    
+        """
+        Test display value for invalid find name input.
+        """
+    
         result = display_find_name("invalid_case_id")
         self.assertEqual(result, "Invalid U.S. Citation ID")
 
-    # Unit tests for display_find_justice_votes function
+
     def test_display_find_justice_votes_valid(self):
+    
+        """
+        Test correct display value for find justice votes function.
+        """
+    
         result = display_find_justice_votes("410 U.S. 113")
         expected_output = 'WODouglas - 3<br>PStewart - 3<br>TMarshall - 1<br>WJBrennan - 1<br>BRWhite - 2<br>WEBurger - 3<br>HABlackmun - 1<br>LFPowell - 1<br>WHRehnquist - 2<br>'
         self.assertIn(expected_output, result)
 
+
     def test_display_find_justice_votes_invalid(self):
+    
+        """
+        Test display value for invalid find justice votes input.
+        """
+    
         result = display_find_justice_votes("invalid_case_id")
         self.assertEqual(result, "Invalid U.S. Citation ID")
 
-    # Integration tests for Flask routes
+
     def test_homepage(self):
+    
+        """
+        Test that necessary information is on the homepage.
+        """
+        
         response = self.app.get('/')
         self.assertIn(b"Supreme Court Data", response.data)
         self.assertIn(b"Usage Instructions", response.data)
+        
 
     def test_display_supreme_court_data_find_name_valid(self):
+        
+        """
+        Test that the app route returns the correct information for the find name function.
+        """
+    
         response = self.app.get('/find_name/410%20U.S.%20113')
         self.assertIn(b"ROE et al. v. WADE, DISTRICT ATTORNEY OF DALLAS COUNTY", response.data) 
 
+
     def test_display_supreme_court_data_find_name_invalid(self):
+    
+        """
+        Test that the app route returns an error for invalid case name for the find name function.
+        """
+    
         response = self.app.get('/find_name/invalid_case_id')
         self.assertIn(b"Invalid U.S. Citation ID", response.data)
 
+
     def test_display_supreme_court_data_find_justice_votes_valid(self):
+
+        """
+        Test that the app route returns the correct information for the find justice votes function.
+        """
+    
         response = self.app.get('/find_justice_votes/410%20U.S.%20113')
         self.assertIn(b'WODouglas - 3<br>PStewart - 3<br>TMarshall - 1<br>WJBrennan - 1<br>BRWhite - 2<br>WEBurger - 3<br>HABlackmun - 1<br>LFPowell - 1<br>WHRehnquist - 2<br>', response.data) 
 
     def test_display_supreme_court_data_find_justice_votes_invalid(self):
+    
+        """
+        Test that the app route returns an error for invalid case name for the find justice votes function.
+        """
+    
         response = self.app.get('/find_justice_votes/invalid_case_id')
         self.assertIn(b"Invalid U.S. Citation ID", response.data)
 
     def test_page_not_found(self):
+    
+        """
+        Test that an error message is displayed for error 404.
+        """
+    
         response = self.app.get('/nonexistent_route')
         self.assertIn(b"Page not found.", response.data)
 
 if __name__ == '__main__':
+
     unittest.main()
