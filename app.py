d82d21b3bbf1c5e8b966d0471de569e045f5b856
unknown <dwuarsidawna@protonmail.com>
2023-10-23 16:20:15 -0500

    added newest function to app

--- app.py
+++ app.py
@@ -10,9 +10,10 @@ def homepage():
     title = "Supreme Court Data"
     welcome_message = "Welcome to Supreme Court Database"
     website_description = "The features of this website are listed below."
-    Find_case_name = "Find Case Name for Case"
-    Find_justice_vote = "Find Justice Votes for Case"
-    Find_case_identifier = "Find all Identifiers for Case"
+    Find_case_name = "Find case name for case"
+    Find_justice_vote = "Find justice votes for case"
+    Find_case_identifier = "Find all identifiers for case"
+    Find_all_justice_votes = "Find all votes for one justice"
     About = "About Us"
     About_message = "This is Group Deliverable 3 for Team F"
     Developers = "Team F"
@@ -23,7 +24,8 @@ def homepage():
                            Find_case_name=Find_case_name,
                            Find_justice_vote=Find_justice_vote,
                            Find_case_identifier=Find_case_identifier,
-                           About = About,
+                           Find_all_justice_votes=Find_all_justice_votes,
+                           About=About,
                            About_message=About_message,
                            Developers=Developers)
     
@@ -81,6 +83,22 @@ def display_find_case_ids(case_id):
         for key, value in ids.items():
             display_ids += key+": "+value+"\n"
         return display_ids[:-1]
+        
+        
+def display_find_all_justice_votes(justice):
+
+    try:
+        votes = all_justice_votes(justice)
+        
+    except LookupError:
+        return "Invalid Justice Name"
+    
+    else:
+        display_votes = ""
+        for case in votes:
+            display_votes += " - ".join(case)
+            display_votes += "\n"
+        return display_votes[:-1]
 
 @app.route('/<function>', methods=['GET'], strict_slashes=False)
 def came_name_displayer_page(function):
@@ -93,7 +111,7 @@ def came_name_displayer_page(function):
     func_text = ("Find "+(function.replace('_', ' ')).title()+" by Case ID")
     func_url = "/"+function
     
-    if function != "case_name" and function != "justice_votes" and function != "case_identifiers":
+    if function not in ("case_name","justice_votes","case_identifiers","all_justice_votes"):
         return "404"
     
     if search_query == "" or search_query == None:
@@ -104,6 +122,8 @@ def came_name_displayer_page(function):
         case_name_text = display_find_justice_votes(search_query)
     elif function == "case_identifiers":
         case_name_text = display_find_case_ids(search_query)
+    elif function == "all_justice_votes":
+        case_name_text = display_find_all_justice_votes(search_query)
     
     return render_template('case_name_displayer.html', case_name_text=case_name_text, function_text=func_text, function_url=func_url)
     
