5dfa8d537eb74e18fbfee0d8a46955bdb9c1a913
unknown <dwuarsidawna@protonmail.com>
2023-10-23 21:43:02 -0500

    added docstrings and separated functinos

--- app.py
+++ app.py
@@ -6,7 +6,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    # Define intermediate variables
+
+    """
+    Displays homepage for the website.
+    """
+
     title = "Supreme Court Data"
     welcome_message = "Welcome to Supreme Court Database"
     website_description = "The features of this website are listed below."
@@ -17,7 +21,7 @@ def homepage():
     About = "About Us"
     About_message = "This is Group Deliverable 3 for Team F"
     Developers = "Team F"
-    # Render the HTML template
+
     return render_template('homepage.html', title=title,
                            welcome_message=welcome_message,
                            website_description=website_description,
@@ -87,6 +91,10 @@ def display_find_case_ids(case_id):
         
 def display_find_all_justice_votes(justice):
 
+    """
+    Calls function from ProductionCode to find, format, and return all justice voting data. Displays error message if an invalid ID was given.
+    """
+
     try:
         votes = all_justice_votes(justice)
         
@@ -99,12 +107,30 @@ def display_find_all_justice_votes(justice):
             display_votes += " - ".join(case)
             display_votes += "\n"
         return display_votes[:-1]
+        
+def search_displayer_logic(function, search_query):
+
+    """
+    Handles logic for text on the search pages.
+    """
+
+    if search_query == "" or search_query == None:
+        return "Case name example: 329 U.S. 40\nJustice name example: HHBurton"
+    elif function == "case_name":
+        return display_find_name(search_query)
+    elif function == "justice_votes":
+        return display_find_justice_votes(search_query)
+    elif function == "case_identifiers":
+        return display_find_case_ids(search_query)
+    elif function == "all_justice_votes":
+        return display_find_all_justice_votes(search_query)
+
 
 @app.route('/<function>', methods=['GET'], strict_slashes=False)
 def came_name_displayer_page(function):
 
     '''
-    Split page for case_name_finder feature with input case id
+    Displays all search pages for features.
     '''
         
     if function not in ("case_name","justice_votes","case_identifiers","all_justice_votes"):
@@ -123,17 +149,7 @@ def came_name_displayer_page(function):
 
     func_text = ("Find "+(function.replace('_', ' ')).title()+search_term)
 
-    
-    if search_query == "" or search_query == None:
-        case_name_text="Case name example: 329 U.S. 40\nJustice name example: HHBurton"
-    elif function == "case_name":
-        case_name_text = display_find_name(search_query)
-    elif function == "justice_votes":
-        case_name_text = display_find_justice_votes(search_query)
-    elif function == "case_identifiers":
-        case_name_text = display_find_case_ids(search_query)
-    elif function == "all_justice_votes":
-        case_name_text = display_find_all_justice_votes(search_query)
+    case_name_text = search_displayer_logic(function, search_query)
     
     return render_template('case_name_displayer.html', case_name_text=case_name_text, 
                            function_text=func_text, function_url=func_url,
