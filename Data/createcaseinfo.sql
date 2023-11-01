41e1a232533ed4be8502af6a64ed46bd4c2510b3
unknown <dwuarsidawna@protonmail.com>
2023-11-01 18:54:34 -0500

    added sql scripts

---
+++ Data/createcaseinfo.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS caseinfo;
+CREATE TABLE caseinfo (
+	usCite VARCHAR,
+	sctCite VARCHAR,
+	ledCite VARCHAR,
+	lexisCite VARCHAR,
+	caseName VARCHAR,
+	dateDecision VARCHAR,
+	caseDisposition CHAR
+);
---
+++ Data/createvoteinfo.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS voteinfo;
+CREATE TABLE voteinfo (
+	lexisCite VARCHAR,
+	justiceName VARCHAR,
+	vote CHAR
+);
