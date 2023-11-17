e862196f05d9b968bf4e4ff214b1c6c06626f419
Ecstonn <gaoy2@carleton.edu>
2023-11-16 20:03:45 -0600

    .

--- app.py
+++ app.py
@@ -148,13 +148,15 @@ def justice_votes_displayer_page():
     dropdown = create_case_id_dropdown()
     
     if search_query == None or search_query == "":
-        case_name_text = "Select case ID from the dropdown menu"         
+        case_name_text = "Select case ID from the dropdown menu"    
+        contextual_info = "This page allows you to see the votes of justices of a certain case"     
     else:
         case_name_text = display_find_justice_votes(search_query)    
+        contextual_info = "On the left is the name of the justice, and on the right is what their vote was."
 
     return render_template('case_justice_vote_displayer.html', case_name_text=case_name_text, 
                            function_text=func_text, function_url=func_url,
-                           query_text=query_text,dropdown=dropdown)
+                           query_text=query_text,dropdown=dropdown,contextual_info=contextual_info)
                            
                            
 @app.route('/case_identifiers', methods=['GET'], strict_slashes=False)
