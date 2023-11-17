77d7d01ac1619eb73040cdbe447e579b7a7f413b
Ecstonn <gaoy2@carleton.edu>
2023-11-16 20:29:51 -0600

    .

--- app.py
+++ app.py
@@ -126,10 +126,10 @@ def came_name_displayer_page():
     
     if search_query == None or search_query == "":
         case_name_text = "Select case ID from the dropdown menu"
-        contextual_info = "This page allows you to see all of the other Identifiers of the case, use them to find this case in case you are using other databases"
+        contextual_info = "This page allows you to check the case name of a case"
     else:
         case_name_text = display_find_name(search_query)     
-        contextual_info = "The identifiers are shown as follows"
+        contextual_info = "The name of the case you selected is:"
     
     return render_template('case_name_displayer.html', case_name_text=case_name_text, 
                            function_text=func_text, function_url=func_url,
@@ -177,11 +177,11 @@ def case_identifiers_displayer_page():
     
     if search_query == None or search_query == "":
         case_name_text = "Select case ID from the dropdown menu" 
-        contextual_info = "This page allows you to check the case name of a case"
+        contextual_info = "This page allows you to see all of the other Identifiers of the case, use them to find this case in case you are using other databases"
                 
     else:
         case_name_text = display_find_case_ids(search_query)   
-        contextual_info = "The name of the case you selected is:"
+        contextual_info = "The identifiers are shown as follows"
 
     return render_template('case_identifier_displayer.html', case_name_text=case_name_text, 
                            function_text=func_text, function_url=func_url,
